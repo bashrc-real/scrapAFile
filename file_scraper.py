20a9098c4fa7f02d0ef1d4a2f0342364d6713a94
bashrc-real <saurabh8c@gmail.com>
2015-10-24 02:09:53 +0530

    Modified to take max_depth and output file location as input 	modified:   file_scraper.py

--- file_scraper.py
+++ file_scraper.py
@@ -17,7 +17,8 @@ DEFAULT_NUMBER_OF_THREADS = 4
 VisitedURL = set()
 keywords = []
 q = deque()
-    
+default_path = "./url.txt"
+global output    
 def TryReadUrl(url):
   try:
       user_agents = ["Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; Avant Browser; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)"
@@ -52,56 +53,56 @@ def TryReadUrl(url):
 
 
 def do_parsing(data):
-    print(threading.current_thread())
-    url, depth = data
-    global q
-    global VisitedURL
-    global keywords
-    idx = url.find("://")
-    assert idx!=-1
-    endidx = url.find("/",idx+len("://"))
-    if endidx!=-1:
-      baseURL=url[0:endidx+1]
-    else:
-      baseURL = url
-    fr = TryReadUrl(url)
-    if  not fr:
-      return
-    pageData = str(fr.read())
-    for key in keywords:
-      if pageData.find(key):
-        #print(url)
-        break
-    idx = 0
-    while idx!=-1:
-      patToSearch = "href=\""
-      idx = pageData.find(patToSearch,idx)
-      if idx!=-1:
-        idx = idx+ len(patToSearch)
-        endidx = pageData.find("\"",idx)
-        nexturl = pageData[idx:endidx]
-        idx = endidx+1
-        for key in keywords:
-          key = "." + key
-          innerIdx = nexturl.find(key);
-          outerIdx = nexturl.rfind("http",0,innerIdx)
-          if innerIdx !=-1 and outerIdx != -1:
-            nexturl = nexturl[outerIdx:innerIdx] +key
-            if nexturl not in VisitedURL:
-              VisitedURL.update(nexturl)
-              print(nexturl+"========\n")
-              continue
-        if nexturl.find("http")!=0:
-          nexturl = baseURL+nexturl
-        if nexturl not in VisitedURL:
-          VisitedURL.update(nexturl)
-          q.extend([[nexturl,depth+1]])
-        if len(VisitedURL) > 100000:
-          VisitedURL.clear()
-
+    try:
+      print(threading.current_thread())
+      url, depth = data
+      global q
+      global VisitedURL
+      global keywords
+      global output
+      idx = url.find("://")
+      assert idx!=-1
+      endidx = url.find("/",idx+len("://"))
+      if endidx!=-1:
+        baseURL=url[0:endidx+1]
+      else:
+        baseURL = url
+      fr = TryReadUrl(url)
+      if  not fr:
+        return
+      pageData = str(fr.read())
+      idx = 0
+      while idx!=-1:
+        patToSearch = "href=\""
+        idx = pageData.find(patToSearch,idx)
+        if idx!=-1:
+          idx = idx+ len(patToSearch)
+          endidx = pageData.find("\"",idx)
+          nexturl = pageData[idx:endidx]
+          idx = endidx+1
+          for key in keywords:
+            key = "." + key
+            innerIdx = nexturl.find(key);
+            outerIdx = nexturl.rfind("http",0,innerIdx)
+            if innerIdx !=-1 and outerIdx != -1:
+              nexturl = nexturl[outerIdx:innerIdx] +key
+              if nexturl not in VisitedURL:
+                VisitedURL.update(nexturl)
+                output.write(nexturl+"\n")
+                continue
+          if nexturl.find("http")!=0:
+            nexturl = baseURL+nexturl
+          if nexturl not in VisitedURL:
+            VisitedURL.update(nexturl)
+            q.extend([[nexturl,depth+1]])
+          if len(VisitedURL) > 100000:
+            VisitedURL.clear()
+    except Exception:
+       pass       
 def crawl_from_url(pool):
   global DEFAULT_NUMBER_OF_THREADS
   global q
+  global output
   while len(q):
     urlsToMap = []
     number_of_urls = min(len(q), DEFAULT_NUMBER_OF_THREADS)
@@ -112,20 +113,29 @@ def crawl_from_url(pool):
         urlsToMap.append((url,depth))
     for result in pool.map(do_parsing, urlsToMap):
       result = result
-           
+    output.flush()
+    os.fsync(output.fileno())       
 def main(arguments):
     global DEFAULT_NUMBER_OF_THREADS
     global keywords
     global VisitedURL
+    global output
+    global default_path
+    global DEFAULT_MAX_DEPTH
     parser = argparse.ArgumentParser(description='Scrape the web from a seed url to find the documents matching particular keyword')
     parser.add_argument('keywords', metavar='N', type=str, nargs='+',help='keywords to look for')
     parser.add_argument('-S', type=str,required=True, help="seed url")
     parser.add_argument('-t', type=int, help="number of threads, default = " + str(DEFAULT_NUMBER_OF_THREADS), default=DEFAULT_NUMBER_OF_THREADS)
+    parser.add_argument('-path', type=str, help="path to the output file , default = "+default_path, default=default_path)
+    parser.add_argument('-depth', type=int, help="depth to which need to visit , default = "+str(DEFAULT_MAX_DEPTH), default=DEFAULT_MAX_DEPTH)
     arguments = parser.parse_args()
     DEFAULT_NUMBER_OF_THREADS = arguments.t
     keywords = arguments.keywords
+    DEFAULT_MAX_DEPTH = arguments.depth
     q.append((arguments.S, 0))
     pool = concurrent.futures.thread.ThreadPoolExecutor(max_workers=DEFAULT_NUMBER_OF_THREADS)
+    default_path = arguments.path
+    output = open(default_path, 'w')
     crawl_from_url(pool)
     
 if __name__ == "__main__":
